baf950a94b48f3279d84da9ec45195915f3091e1
dastangoo <hamed.dastangoo@gmail.com>
2018-03-24 19:58:18 +0430

    8

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,24 +12,46 @@ module.exports = function(grunt) {
           src:['test/integration/**/*.js']
         }
       },
+      shell: {
+        coverage: {
+          command: 'babel-node node_modules/.bin/isparta cover _mocha -- test/**/*.js'
+        }
+      },
+      open: {
+        coverage: {
+          path: 'coverage/lcov-report/index.html'
+          // app: 'Google Chrome'
+        }
+      },
       sass: {
         options: {
           sourceMap: true,
           outputStyle: "compressed"
         },
         compile: {
-          files: {
-            'public/css/foodprint.css': 'public/scss/footprint.scss'
-          }
+            files: {
+              'public/css/foodprint.min.css':'public/scss/footprint.scss'
+            }
         }
       },
       eslint: {
         all: ['src/*.js', '!/node_modules/**/*.js']
+      },
+      clean: {
+        coverage: ['./coverage']
       }
+      
     });
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    
     
     grunt.registerTask('test', 'run automated tests', ['mochaTest:unit', 'mochaTest:integration']);
+    grunt.registerTask('coverage', 'Generate coverage report', ['clean', 'shell', 'open']);
 };
 
