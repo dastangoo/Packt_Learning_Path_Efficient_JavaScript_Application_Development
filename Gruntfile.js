201f9d2408c4a33eb5136007cf0265f581b5513b
dastangoo <hamed.dastangoo@gmail.com>
2018-03-24 18:43:50 +0430

    6

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,9 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        sass: {
-            compile: {
-                files: {
-                    'public/css/foodprint.min.css': ['public/scss/foodprint.scss']
-                },
-                options: {
-                    sourceMap: true,
-                    outputStyle: 'compressed'
-                }
-            }
-        }
+      eslint: {
+        all: ['src/*.js', '!/node_modules/**/*.js']
+      }
     });
-    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-eslint');
 };
 
