c2749b4dad53ae860738ed6b75d3257baa17a168
dastangoo <hamed.dastangoo@gmail.com>
2018-03-25 19:48:48 +0430

    33

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express from server directory' });
 });
 
+/* GET login page */
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Login Page', message: req.flash('loginMessage')});
+});
+
+/* GET signup page */
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: 'Signup Page', message: req.flash('signupMessage')});
+});
+
+/* GET profile page */
+router.get('/profile', function(req, res) {
+  res.render('profile', { 
+    title: 'Profile Page', 
+    user: req.user,
+    avatar: gravatar.url(req.user.email, { s: '100', r: 'x', d: 'retro' }, true)
+  });
+});
 module.exports = router;
